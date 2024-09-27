2bfbad0ce6ab1fdfc639c0b5b2f19dc3d04a50fc
mahmut26 <115157615+mahmut26@users.noreply.github.com>
2024-09-28 01:05:44 +0300

    Add files via upload

---
+++ browser.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sudo apt install cmake valac libwebkit2gtk-4.0-dev libgcr-3-dev libpeas-dev libsqlite3-dev libjson-glib-dev libarchive-dev intltool libxml2-utils git
+git clone https://github.com/midori-browser/core.git
