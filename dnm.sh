e18597926ce05b505d095b6c1e0d9d9dceb5ddfd
mahmut26 <115157615+mahmut26@users.noreply.github.com>
2024-09-28 02:20:27 +0300

    Create dnm.sh

---
+++ dnm.sh
@@ -0,0 +1,18 @@
+
+sudo apt install cmake 
+sudo apt install valac 
+sudo apt install libwebkit2gtk-4.0-dev 
+sudo apt install libgcr-3-dev 
+sudo apt install libpeas-dev 
+sudo apt install libsqlite3-dev 
+sudo apt install libjson-glib-dev 
+sudo apt install libarchive-dev 
+sudo apt install intltool 
+sudo apt install libxml2-utils
+git clone https://github.com/midori-browser/core/releases/download/v9.0/midori-v9.0.tar.gz
+cd midori-v9.0
+mkdir _build
+cd _build
+cmake -DCMAKE_INSTALL_PREFIX=/usr ..
+make
+sudo make install
