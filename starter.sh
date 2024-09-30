e0571cb8c03a0d87980fd2a791177a02fd9d9e60
mahmut26 <115157615+mahmut26@users.noreply.github.com>
2024-10-01 01:16:38 +0300

    Update starter.sh

--- starter.sh
+++ starter.sh
@@ -1,4 +1,6 @@
 cd MagicMirror
 npm run server &
 
-sudo xinit /usr/bin/midori -e Fullscreen - a http://localhost:8080
+sudo xinit /usr/bin/midori -e Fullscreen - a http://localhost:8080 &
+
+python3 ./reboot.py
