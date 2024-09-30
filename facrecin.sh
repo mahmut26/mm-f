1b1550a68a0233b8dc3401dfabcf31960f887676
mahmut26 <115157615+mahmut26@users.noreply.github.com>
2024-10-01 01:34:10 +0300

    Create facrecin.sh

---
+++ facrecin.sh
@@ -0,0 +1,29 @@
+sudo apt-get install cmake
+sudo apt-get install gfortran 
+sudo apt-get install git 
+sudo apt-get install wget 
+sudo apt-get install curl 
+sudo apt-get install graphicsmagick 
+sudo apt-get install libgraphicsmagick1-dev
+sudo apt-get install libatlas-base-dev
+sudo apt-get install liblapack-dev
+sudo apt-get install libatlas3-base 
+sudo apt-get install libavcodec-dev 
+sudo apt-get install libavformat-dev 
+sudo apt-get install libboost-all-dev
+sudo apt-get install libgtk2.0-dev 
+sudo apt-get install libjpeg-dev 
+sudo apt-get install liblapack-dev
+sudo apt-get install libswscale-dev 
+sudo apt-get install pkg-config 
+sudo apt-get install python3-dev
+sudo apt-get install python3-numpy
+sudo apt-get install python3-pip
+sudo apt-get install zip
+sudo apt-get install libopenblas-dev
+sudo pip3 install dlib
+
+sudo pip3 install --no-cache-dir face_recognition
+
+git clone --single-branch https://github.com/ageitgey/face_recognition.git
+
