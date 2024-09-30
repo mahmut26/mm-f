21dbfd9802af1c61b467b4f8b638440a024ac1d2
mahmut26 <115157615+mahmut26@users.noreply.github.com>
2024-10-01 01:12:38 +0300

    Update face.py

--- face.py
+++ face.py
@@ -1,61 +1,111 @@
-import cv2
-import numpy as np
+import RPi.GPIO as GPIO
 import face_recognition
+import picamera
+import numpy as np
 import os
 
-import webbrowser
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23,GPIO.IN)
+GPIO.setup(17,GPIO.OUT)
 
+GPIO.output(17,0)
+while True:
+ifGPIO.input(23)==1:
+whileGPIO.input(23)==0:
+camera = picamera.PiCamera()
+camera.resolution = (320, 240)
+output = np.empty((240, 320, 3), dtype=np.uint8)
 
-video_capture = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 face_locations = []
 face_encodings = []
 face_names = []
+
 known_person=[]
 known_image=[]
 known_face_encoding=[]
 
-for file in os.listdir("friends"):
-    try:
-        #Extracting person name from the image filename eg: david.jpg
-        known_person.append(file.replace(".jpg", ""))
-        file=os.path.join("friends/", file)
-        known_image = face_recognition.load_image_file(file)
-        known_face_encoding.append(face_recognition.face_encodings(known_image)[0])
-    except Exception as e:
-        pass
-
-
+for file in os.listdir("kullanicilar"):
+try:
+known_person.append(file.replace(".jpg", ""))
+file=os.path.join("kullanicilar/", file)
+known_image = face_recognition.load_image_file(file)
+known_face_encoding.append(face_recognition.face_encodings(known_image)[0])
+exceptException as e:
+pass
 while True:
-    ret, frame = video_capture.read()
-    rgb_frame = frame[:, :, ::-1]
-    face_locations = face_recognition.face_locations(rgb_frame)
-    face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
-    for face_encoding in face_encodings:
 
-        match = face_recognition.compare_faces(known_face_encoding, face_encoding)
-        matches = np.where(match)[0]
-        if len(matches) > 0:
-            name = str(known_person[matches[0]])
-            print("I see someone named {}!".format(name))
+camera.capture(output, format="rgb")
+face_locations = face_recognition.face_locations(output)
+face_encodings = face_recognition.face_encodings(output, face_locations)
+forface_encoding in face_encodings:
+						
+match = face_recognition.compare_faces(known_face_encoding, face_encoding)
+matches=np.where(match)[0] #Checking whichimage is matched
+							
+iflen(matches)>0:
+name = str(known_person[matches[0]])
+print("I seesomeonenamed {}!".format(name))
+if '1' in name:
+GPIO.output(17,1)
+camera.close()
+os.system("cd /home/ayna/Desktop")
+os.system('./1.sh')
+										
+elif '2' in name:
+GPIO.output(17,1)
+										
+camera.close()
+os.system("cd /home/ayna/Desktop")
+os.system('./2.sh')
 
-            if 'ma' in name:
-                exec(open("dosyama.py").read())
-                exit()
+elif '3' in name:
+GPIO.output(17,1)
+										
+camera.close()
+os.system("cd /home/ayna/Desktop")
+os.system('./3.sh')
 
+elif '4' in name:
+GPIO.output(17,1)
+										
+camera.close()
+os.system("cd /home/ayna/Desktop")
+os.system('./4.sh')
 
+elif '5' in name:
+GPIO.output(17,1)
+										
+camera.close()
+os.system("cd /home/ayna/Desktop")
+os.system('./5.sh')
 
+elif '6' in name:
+GPIO.output(17,1)
+										
+camera.close()
+os.system("cd /home/ayna/Desktop")
+os.system('./6.sh')
 
+elif '7' in name:
+GPIO.output(17,1)
+										
+camera.close()
+os.system("cd /home/ayna/Desktop")
+os.system('./7.sh')
 
-            elif 'biden' in name:
-                exec(open("biden.py").read())
-                quit()
+elif '8' in name:
+GPIO.output(17,1)
+										
+camera.close()
+os.system("cd /home/ayna/Desktop")
+os.system('./8.sh')
 
-                cv2.imshow('Video', frame)
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    quit()
-                video_capture.release()
-                cv2.destroyAllWindows()
-
+elif '9' in name:
+GPIO.output(17,1)
+										
+camera.close()
+os.system("cd /home/ayna/Desktop")
+os.system('./9.sh')
 
 
 
